496210e1e7a4e05e9a340eb5737ca452f0585c0e
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-09-27 08:32:57 -0600

    manejo de funciones

--- intro-js/js/04-funciones.js
+++ intro-js/js/04-funciones.js
@@ -1,13 +1,27 @@
 
 // definiendo una función
 function saludo() {
-    console.log('Hola desde una función');
+    console.log('Hola desde una función')
 }
 
 const saludo2 = function () {
-
-    console.log('Hola desde una función');
+    console.log('Hola desde una función')
 }
+
+const imprimir = (nombre = '') => {
+    console.log('Hola desde una función de flecha ' + nombre)
+}
+
+const areaCirculo = (r) => {
+
+    if (!Number(r)) {
+        return 0
+    }
+
+    return Math.PI * Math.pow(r, 2)
+}
+
+const areaCirculo2 = (r) => Math.PI * Math.pow(r, 2)
 
 // llamando a la función
 console.log(saludo());
@@ -15,3 +29,4 @@ console.log(saludo());
 const miFuncion = saludo;
 
 miFuncion()
+imprimir('Juan') // <- hace falta el parametro
