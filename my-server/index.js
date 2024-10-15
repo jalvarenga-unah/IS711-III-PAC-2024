45bf99f65f273b9e9a38fa306db6289a94c529de
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-10-15 17:55:58 -0600

    Manejo de verbos y url

--- my-server/index.js
+++ my-server/index.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import users from './stores/users.json' with { type: 'json' };
 
 // establecer un puerto
 
@@ -6,10 +7,41 @@ const PORT = process.env.PORT || 3000
 
 //crear el servidor
 
+//req,res
 const app = http.createServer((request, response) => {
 
     // TODO: 
-    //Manejo de metodos de peticion
+    //Manejo de metodos (Verbos) de peticion
+
+    const { method, url } = request
+
+    console.log(url)
+
+    switch (method) {
+        case 'GET':
+
+            switch (url) {
+                case '/users':
+                    response.statusCode = 200 // Es por defecto si no se pone
+                    response.setHeader('Content-Type', 'application/json')
+                    response.end(JSON.stringify(users))
+                    break
+                default:
+                    response.end('Soy una peticion GET')
+            }
+            break
+        case 'POST':
+            response.end('Soy una peticion POST')
+            break
+        default:
+            response.end('Soy una peticion que no es GET ni POST')
+            break
+    }
+
+
+
+
+
     // codigos de respuestas
     // encabezados de la respuesta
 
@@ -17,7 +49,7 @@ const app = http.createServer((request, response) => {
 
     //en funcion de lo que el cliente solicite, se da una respuesta
     console.log('Holaaa desde el servidor')
-    response.end('Hola mundo!!') //servirle el contenido al cliente
+    // response.end('Hola mundo!!') //servirle el contenido al cliente
 
 })
 
