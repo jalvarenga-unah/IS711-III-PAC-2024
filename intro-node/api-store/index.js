2fbe554b0e7483af129e5d97cb04901c2292f8e1
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-10-23 17:50:31 -0600

    Middelwares en express

--- intro-node/api-store/index.js
+++ intro-node/api-store/index.js
@@ -1,7 +1,27 @@
-import express from 'express'
+import express, { json } from 'express'
 import users from './stores/users.json'  with {type: "json"}
 
 const app = express() // instance de express (createServer)
+
+//Middleware
+app.disable('x-powered-by')
+app.use(json()) //Middleware de express para capturar el body de la petición
+// app.use((req, res, next) => {
+
+//     if (req.method != 'POST') next()
+
+//     let body = ''
+
+//     req.on('data', (chunk) => {
+//         body += chunk.toString()
+//     })
+
+//     req.on('end', () => {
+//         req.body = JSON.parse(body)
+//         next()
+//     })
+
+// })
 
 const PORT = process.env.PORT || 3000
 
@@ -15,6 +35,37 @@ app.get('/users', (req, res) => {
         .header('Content-Type', 'application/json')
         .status(200)
         .json(users)
+})
+
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params
+
+    const parse_id = Number(id)
+
+    if (!parse_id) {
+        res
+            .status(400)
+            .json({
+                message: "El valor del ID no es un número"
+            })
+    }
+
+    const user = users.find(user => user.id == id)
+
+    if (!user) {
+        res
+            .json({
+                message: "Usuario no encontrado"
+            })
+    }
+
+    res.json(user)
+
+})
+
+app.post('/users', (req, res) => {
+
+    res.json(req.body)
 })
 
 
