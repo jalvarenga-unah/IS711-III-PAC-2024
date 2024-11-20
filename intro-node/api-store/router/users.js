6a11d9bd7c3fcfae5b1489d188ba1cd9ad6f37eb
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-11-20 17:54:14 -0600

    protección de rutas

--- intro-node/api-store/router/users.js
+++ intro-node/api-store/router/users.js
@@ -1,16 +1,22 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/user-controller.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
+import isAdmin from '../middlewares/isAdmin.js'
 
 const userRouter = Router()
 
-// /users
-userRouter.get('/', UserController.getAllUsers)
+// se aplica el middleware de autenticación a todas las rutas de este router
+userRouter.use(authMiddleware)
+
+// si quiero que una ruta tenga un middleware en específico,
+//  se lo paso como segundo argumento
+userRouter.get('/', /*authMiddleware,*/ UserController.getAllUsers)
 
 userRouter.get('/:user_id', UserController.getUserById)
 
-userRouter.post('/', UserController.createUser)
+userRouter.post('/', isAdmin, UserController.createUser)
 
-userRouter.patch('/:id', UserController.updateUser)
+userRouter.patch('/:id', isAdmin, UserController.updateUser)
 
 // userRouter.delete('/:id', (req, res) => {
 //     //.....
